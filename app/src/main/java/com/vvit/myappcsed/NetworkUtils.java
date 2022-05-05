e27955cb74a32fc84d679ead0b228e3cdb80d76a
Sriharsha1103 <sriharsha1103@gmail.com>
2022-05-05 13:27:40 +0530

    Modified NetworkUtils

--- app/src/main/java/com/vvit/myappcsed/NetworkUtils.java
+++ app/src/main/java/com/vvit/myappcsed/NetworkUtils.java
@@ -10,30 +10,7 @@ import java.util.Scanner;
 
 public class NetworkUtils {
     public static String HTTP_URL = "https://api.themoviedb.org/3/movie/popular?api_key=d696f7a5f93540b0dac2ed52b5b7ee93";
-    public static String GET_URL = "https://api.themoviedb.org/3";
-    public static String NAME = "movie";
-    public static String API_KEY = "api_key";
-    public static String IMAGE_URL = "http://image.tmdb.org/t/p/w300";
-    public static URL url=null;
-    public static URL buildURL(String query,Boolean value){
-        Uri buildUri;
-        if(value) {
-            buildUri = Uri.parse(GET_URL).buildUpon()
-                    .appendPath(NAME)
-                    .appendPath(query)
-                    .appendQueryParameter(API_KEY, "d696f7a5f93540b0dac2ed52b5b7ee93").build();
-        }
-        else{
-            buildUri = Uri.parse(IMAGE_URL).buildUpon()
-                    .appendPath(query).build();
-        }
-        try {
-            url = new URL(buildUri.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return url;
-    }
+
     public static String getResponseFromHttpUrl()
             throws IOException{
         Uri uri = Uri.parse(HTTP_URL).buildUpon().build();
